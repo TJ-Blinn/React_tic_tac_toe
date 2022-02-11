648fef912d4c17aefec6f595f32c6feacd5f9ba6
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-02-11 10:56:58 -0500

    Added fragment for h1, add return for Board component, info-wrapper (CSS), h3 label for history, h3 statement for winner or next player

--- src/components/Game.js
+++ src/components/Game.js
@@ -26,4 +26,20 @@ const Game = () => {
     setStepNumber(historyPoint.length);
     setXisNext(!xIsNext);
   };
+
+  return (
+    <>
+      <h1>React Tic Tac Toe - With Hooks</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="info-wrapper">
+        <div>
+          <h3>History</h3>
+          {renderMoves()}
+        </div>
+        <h3>{winner ? "Winner: " + winner : "Next Player: " + xO} </h3>
+      </div>
+    </>
+  );
 };
+
+export default Game;
